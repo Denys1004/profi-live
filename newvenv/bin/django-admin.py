7dc7fa474d5f47f84fce5e2e37d5e33c11fd0c64
denys krychevskyi <krychevskyidenys@gmail.com>
2020-08-01 21:34:34 -0700

    update gitignore

--- newvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/den/Desktop/PROJECTS/DEPLOYED/Profi_Project/profi/newvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
